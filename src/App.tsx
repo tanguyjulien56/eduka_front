b6f023fd480bf2f89e9a9d01f55eb45a00bd7eb7
Sylvain <sylvain.de.luca@gmail.com>
2024-04-30 15:04:03 +0200

    maj route

--- src/App.tsx
+++ src/App.tsx
@@ -12,11 +12,11 @@ import EventsPage from "./pages/Event/EventsPage";
 import EventsUserPage from "./pages/Event/EventsUserPage";
 import HomePageParent from "./pages/Home/HomePageParent";
 import NotificationPage from "./pages/Notification/NotificationPage";
-import ProfilPage from "./pages/Profil/ProfilPage";
+
 import SearchDetailPage from "./pages/Search/SearchDetailPage";
 import SearchPage from "./pages/Search/SearchPage";
 import { ModalProvider } from "./services/Context/ModalContext";
-import NotFoundPage from "./utils/NotFoundPage";
+
 
 
 
@@ -29,17 +29,17 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePageParent />} />
           <Route path="/" element={<HomePageSchool />} />
-          <Route path="/SchoolCreateUser" element={<SchoolCreateUser />} />
-          <Route path="/ParentSignUpPage" element={<ParentSignUpPage />} />
-          <Route path="/TeacherSignUpPage" element={<TeacherSignUpPage />} />
-          <Route path="/ParentTeacherSignUpPage" element={<ParentTeacherSignUp />} />
+          <Route path="/school_create_user" element={<SchoolCreateUser />} />
+          <Route path="/parent_signUpPage" element={<ParentSignUpPage />} />
+          <Route path="/teacherSignUpPage" element={<TeacherSignUpPage />} />
+          <Route path="/parentTeacherSignUpPage" element={<ParentTeacherSignUp />} />
           <Route path="/notification" element={<NotificationPage />} />
           <Route path="/search" element={<SearchPage />} />
-          <Route path="/profil" element={<ProfilPage />} />
+          
           <Route path="/for_you_event" element={<EventsUserPage />} />
           <Route path="/all_events" element={<EventsPage />} />
           <Route path="/search_detail/:id" element={<SearchDetailPage />} />
-          <Route path="*" element={<NotFoundPage />} />
+          {/* <Route path="*" element={<NotFoundPage />} /> */}
         </Routes>
       </ModalProvider>
 
